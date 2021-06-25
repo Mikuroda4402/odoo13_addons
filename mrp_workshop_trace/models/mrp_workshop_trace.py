e90b4992a0c08f556e95c1d52a83034c8bdae4d9
Mikuroda <mainfire41@gmail.com>
2021-06-25 23:40:23 +0800

    調整 追蹤製造工單可查詢工單單號，以及顯示文字

--- mrp_workshop_trace/models/mrp_workshop_trace.py
+++ mrp_workshop_trace/models/mrp_workshop_trace.py
@@ -8,9 +8,9 @@ from dateutil.relativedelta import relativedelta
 class MrpWorkshopTrace(models.TransientModel):
     _name = 'mrp.workshop.trace'
 
-    mrp_no = fields.Char(string='製造單號')
+    mrp_no = fields.Char(string='製造單號/工單單號')
     workorder_ids = fields.Many2many('mrp.workorder', string='工單清單')
-    mrp_exist = fields.Boolean(string='Search mrp exist?')
+    workerorder_exist = fields.Boolean(string='Search mrp exist?')
 
     @api.onchange('mrp_no')
     def search_mrp(self):
@@ -18,8 +18,12 @@ class MrpWorkshopTrace(models.TransientModel):
         production_id = 0
 
         if name:
-            production = self.env['mrp.production'].search([('name', 'like', f'%{name}%')], limit=1, order='name')
+            production = self.env['mrp.production'].search([('name', 'like', f'{name}%')], limit=1, order='name')
             production_id = production.id if any(production) else 0
 
-        self.workorder_ids = self.env['mrp.workorder'].search([('production_id', '=', production_id)], order='id')
-        self.mrp_exist = True if production_id else False
+        if production_id:
+            self.workorder_ids = self.env['mrp.workorder'].search([('production_id', '=', production_id)], order='id')
+            self.workerorder_exist = True
+        else:
+            self.workorder_ids = self.env['mrp.workorder'].search([('name', '=', name)], order='id')
+            self.workerorder_exist = any(self.workorder_ids)
