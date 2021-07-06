b787bf2595a48465dc66c7f6cc7d4291fc824776
Jason <jaronemo@msn.com>
2021-07-06 08:34:12 +0800

    上傳聯絡工具需要的序號模組

---
+++ sequence_customize/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': '單據序號客製化模組',
+    'version': '12.0.1.0.0',
+    'category': 'sequence',
+    'summary': '序號可選擇每日, 每月, 每年重置',
+    'author': '先傑電腦股份有限公司',
+    'website': 'http://www.win2000.com.tw',
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'views/ir_sequence_customize.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
