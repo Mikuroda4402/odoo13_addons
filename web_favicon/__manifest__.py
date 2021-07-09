9d852090cf82235664281f8afc288f32bcfe6126
JasonWu <jaronemo@msn.com>
2021-07-09 10:00:13 +0800

    加入Web Favicon控制

---
+++ web_favicon/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Web Facicon',
+    'category': 'other',
+    'summary': '自訂網站 Favicon',
+    'version': '1.0',
+    'description': """自訂網站 Favicon""",
+    'author': 'mikuroda',
+    'depends': ['web'],
+    'data': [
+        'views/web_templates.xml',
+    ],
+    'installable': True,
+}
