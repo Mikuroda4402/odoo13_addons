991843cd81dad9c56170b9aa7a4222b809509ee5
at3226 <mainfire41@gmail.com>
2021-07-12 10:35:41 +0800

    補上缺少的模組檔案

---
+++ mrp_routing_subcontracting/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
+from . import purchase_order
+from . import mrp_workorder
+from . import mrp_routing_workcenter
+
